d71e48dbd32700d51bfa3d25fe61df0a64ee1339
Anthony Bertrand <anthony.e.bertrand@gmail.com>
2024-07-30 15:08:53 +0200

    NVIM: Supprime le keybinding leader bloquant les keybinds Neorg 9

--- nvim/init.lua
+++ nvim/init.lua
@@ -548,7 +548,7 @@ vim.o.termguicolors = true
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+-- vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
