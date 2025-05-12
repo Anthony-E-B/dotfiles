f8d121ea09ade841b5ce8bd060b42921176edc53
Anthony E. Bertrand <anthony.e.bertrand@gmail.com>
2025-05-12 08:33:33 -0400

    NVIM: Replace deprecated

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,14 +39,16 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Default tabstop/shiftwidth
-vim.opt.shiftwidth = 2;
-vim.opt.tabstop = 2;
+vim.o.shiftwidth = 2;
+vim.o.tabstop = 2;
 
-vim.opt.scrolloff = 3;
-vim.opt.conceallevel = 2;
+vim.o.scrolloff = 3;
+vim.o.conceallevel = 2;
 
-vim.opt.cursorline = true;
-vim.opt.cursorcolumn = true;
+vim.o.cursorline = true;
+vim.o.cursorcolumn = true;
+
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 function LoadNeorgWorkspace(workspace)
   vim.cmd('Neorg workspace ' .. workspace)
@@ -206,8 +208,8 @@ require('lazy').setup({
     'rmagatti/auto-session',
     config = function()
       require('auto-session').setup({
+        git_use_branch_name = true,
         log_level = "error",
-        auto_session_use_git_branch = true,
       });
       vim.g.auto_session_pre_save_cmds = { "silent! tabdo NvimTreeClose" }
     end,
@@ -501,44 +503,44 @@ vim.keymap.set('n', '<leader>sthd', '<Cmd>lua vim.cmd.colorscheme(DarkThemeName)
 -- [[ Setting options ]]
 
 -- Set highlight on search
-vim.opt.hlsearch = false
+vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
 vim.wo.relativenumber = true
 
 -- Enable mouse mode
-vim.opt.mouse = 'a'
+vim.o.mouse = 'a'
 
-vim.opt.fileformat = "unix"
+vim.o.fileformat = "unix"
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
+vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
-vim.opt.breakindent = true
+vim.o.breakindent = true
 
 -- Save undo history
-vim.opt.undofile = true
+vim.o.undofile = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 -- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
 
 -- Decrease update time
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.opt.completeopt = 'menuone,noselect'
+vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
-vim.opt.termguicolors = true
+vim.o.termguicolors = true
 
 -- [[ Basic Keymaps ]]
 
